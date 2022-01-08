95df401181f9a95bb016aa236f273967a1133a8a
Naoki Seto(Lionas) <lionas.watanabe@gmail.com>
2022-01-09 08:43:32 +0900

    コメント修正

--- app/src/main/java/com/dji/djisdkdemo/interfaces/MainActivityViewControllerCallback.kt
+++ app/src/main/java/com/dji/djisdkdemo/interfaces/MainActivityViewControllerCallback.kt
@@ -1,7 +1,7 @@
 package com.dji.djisdkdemo.interfaces
 
 /**
- * MapControllerからMainActivityViewControllerの処理を呼び出すインタフェース
+ * MapViewControllerからMainActivityViewControllerの処理を呼び出すインタフェース
  */
 interface MainActivityViewControllerCallback {
     fun onMapClick()
